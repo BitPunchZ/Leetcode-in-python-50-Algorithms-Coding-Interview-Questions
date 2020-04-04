d7a05452e9f9eb69f985441a34fbbaf400205637
BitPunchZ <mpayob2000@gmail.com>
2020-04-04 20:12:57 +0200

    fix move zeroes

--- Interview Questions solutions/move zeroes/index.py
+++ Interview Questions solutions/move zeroes/index.py
@@ -1,11 +1,10 @@
 class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
-		j = 0
+        j = 0
+        for num in nums:
+            if(num != 0):
+                nums[j] = num
+                j += 1
 
-		for num in nums:
-			if(num!=0):
-				nums[j] = num
-				j+=1
-
-		for x in range(j,len(n)):
-			nums[x] = 0
+        for x in range(j, len(nums)):
+            nums[x] = 0
