a04f5cbe91642a2b77e4128b7c5869f642ebd85f
BitPunchZ <mpayob2000@gmail.com>
2020-04-21 23:48:42 +0200

    fix count primes

--- Interview Questions solutions/count primes/index.py
+++ Interview Questions solutions/count primes/index.py
@@ -1,13 +1,14 @@
 class Solution:
-    def countPrimes(self, n: int) -> int:
-		if n<2:
-			return 0
-		isPrime = [True] = n
-		isPrime[0] = isPrime[1] = False
-
-		for i in range(2,math.ceil(math.sqrt(n))):
-			if isPrime[i]:
-				for multiples_of_i in range(i*i,n,i):
-					isPrime[multiples_of_i] = False
-
-		return sum(isPrime)
+    def countPrimes(self, n: int) -> int:		
+        
+        if n<2:
+            return 0
+        isPrime = [True]*n
+        isPrime[0] = isPrime[1] = False
+        
+        for i in range(2,math.ceil(math.sqrt(n))):
+            if isPrime[i]:
+                for multiples_of_i in range(i*i,n,i):
+                    isPrime[multiples_of_i] = False
+        
+        return sum(isPrime)
