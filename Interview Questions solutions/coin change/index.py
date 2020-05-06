1d8c811ff285382d7d0b76e7deb9579558d69324
BitPunchZ <mpayob2000@gmail.com>
2020-05-06 13:29:11 +0200

    fix coin change missing code

--- Interview Questions solutions/coin change/index.py
+++ Interview Questions solutions/coin change/index.py
@@ -1,2 +1,19 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
+        if amount <= 0:
+            return 0
+        
+        if min(coins) > amount:
+            return -1
+
+        INT_MAX = 1<<32
+        dp = [INT_MAX] * (amount +1)
+        
+        dp[0] = 0
+        
+        for i in range(1, amount + 1):
+            for coin in coins:
+                if coin <= i:
+                    dp[i] = min((dp[i-coin] + 1), dp[i])
+                    
+        return dp[amount] if dp[amount] != INT_MAX else -1
