c989fdd23f15e5c2cc163eda19095a6583d91d79
BitPunchZ <mpayob2000@gmail.com>
2020-05-06 13:33:43 +0200

    fix unique paths

--- Interview Questions solutions/unique paths/index.py
+++ Interview Questions solutions/unique paths/index.py
@@ -1,16 +1,15 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-
-      dp = [[0 for x in range(m) for y in range(n)]]
-
-      for i in range(m):
-        dp[0][i] = 1
-
-      for i in range(n):
-        dp[i][0] = 1
-
-      for i in range(1,n):
-        for j in range(1,m):
-          dp[i][j] = dp[i-1][j]+dp[i][j-1]
-
-      return dp[-1][-1]
+        
+        dp = [[0 for x in range(m)] for y in range(n)]
+        for i in range(m):
+            dp[0][i] = 1
+        
+        for i in range(n):
+            dp[i][0] = 1
+        
+        for i in range(1, n):
+            for j in range(1, m):
+                dp[i][j] = dp[i-1][j] + dp[i][j-1]
+        
+        return(dp[-1][-1])
