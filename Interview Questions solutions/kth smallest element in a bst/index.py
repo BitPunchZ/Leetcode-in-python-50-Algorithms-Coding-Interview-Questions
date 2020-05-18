fcd863c27d375cb97f83abdba5235e7f7897e847
BitPunchZ <44556781+BitPunchZ@users.noreply.github.com>
2020-05-18 06:07:54 +0200

    New solution for kth smallest element in bst

--- Interview Questions solutions/kth smallest element in a bst/index.py
+++ Interview Questions solutions/kth smallest element in a bst/index.py
@@ -1,21 +1,17 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
-
 class Solution:
-    def countNodes(self,root):
-        return 0 if root is None else 1+self.countNodes(root.left)+self.countNodes(root.right)
-    def dfs(self,root,k):
-        count = self.countNodes(root.left)
-        if(count+1==k):
-            return root.val
-        if(count+1>k):
-            return self.dfs(root.left,k)
-        if(count+1<k):
-            return self.dfs(root.right,k-1-count)
-        return -1
     def kthSmallest(self, root: TreeNode, k: int) -> int:
-        return self.dfs(root,k)
+        self.k = k
+        self.res = None
+        self.helper(root)
+        return self.res
+
+    def helper(self, root):
+        if not root:
+            return
+        self.helper(root.left)
+        
+        self.k -= 1
+        if self.k == 0:
+            self.res = root.val
+            return
+        self.helper(root.right)
