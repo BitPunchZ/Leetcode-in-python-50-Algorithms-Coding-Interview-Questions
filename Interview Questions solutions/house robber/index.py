5f8835294d6cc658825a44cd83e287c71d4daf6d
BitPunchZ <mpayob2000@gmail.com>
2020-06-29 19:20:05 +0200

    tweaks to house robber solution to reflect new video

--- Interview Questions solutions/house robber/index.py
+++ Interview Questions solutions/house robber/index.py
@@ -1,15 +1,18 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
         n = len(nums)
-        if(n == 0):
+        if(n==0):
             return 0
-
+        
         dp = [0] * n
         dp[0] = nums[0]
 
-        for i in range(n):
-            dp[i] = nums[0] if i == 0 else (
-                max(nums[0], nums[1]) if i == 1
-                else max(dp[i-1], dp[i-2]+nums[i])
-            )
-        return dp[n-1]
+        for i in range(1,n):
+            if(i==1):
+                dp[i] = max(nums[0],nums[1])
+            else:
+                dp[i] = max(dp[i-1], dp[i-2]+nums[i])
+            
+        return dp[-1]
+
+        
