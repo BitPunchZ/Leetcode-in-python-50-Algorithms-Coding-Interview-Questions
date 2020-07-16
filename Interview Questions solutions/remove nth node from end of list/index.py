4ea4f3817c1b0314ec006ead423064938ddb72e7
BitPunchZ <44556781+BitPunchZ@users.noreply.github.com>
2020-07-16 06:20:20 +0200

    Update remove nth node from end of list code

--- Interview Questions solutions/remove nth node from end of list/index.py
+++ Interview Questions solutions/remove nth node from end of list/index.py
@@ -7,16 +7,19 @@
 class Solution:
     def removeNthFromEnd(self, head: ListNode, n: int) -> ListNode:
         ans = ListNode(0)
-        first = ans
-        second = ans
         ans.next = head
 
-        for i in range(1,n+1):
+        first = ans
+        second = ans
+
+        for i in range(1,n+2):
             first = first.next
         
-        while(first is not None):
+        while (first is not None):
             first = first.next
             second = second.next
 
         second.next = second.next.next
+        
+
         return ans.next
