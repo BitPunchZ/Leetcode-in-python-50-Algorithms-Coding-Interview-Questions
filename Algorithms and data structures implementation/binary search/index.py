252f1f13e6236d6bffac5866ce4e54e09ebbccf7
BitPunchZ <44556781+BitPunchZ@users.noreply.github.com>
2020-08-31 08:23:14 +0200

    Upgrade binary search code

--- Algorithms and data structures implementation/binary search/index.py
+++ Algorithms and data structures implementation/binary search/index.py
@@ -1,13 +1,11 @@
 
-import math
-
-
 def binarySearch(arr, target):
     left = 0
     right = len(arr)-1
     while left <= right:
 
-        mid = math.floor(left + (right - left)/2)
+        mid = (left+right)//2
+
         # Check if x is present at mid
         if arr[mid] == target:
             return mid
