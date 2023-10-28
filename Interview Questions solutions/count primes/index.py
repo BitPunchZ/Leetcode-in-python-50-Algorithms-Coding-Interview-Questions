46f3fc3849ae3d1aced93cb32a5350376c4d28f7
Damilola Abioye <damyl4sure@gmail.com>
2023-10-28 20:06:24 +0100

    Update index.py
    Where n = 10
    Int(sqrt(10) = 3
    
    for loop i in range(2, 3):
       only caters for i = 2 and never i = 3. So index 9 also needs to be set to False in the isPrime array. (9 is less than 10 and not a prime number)
    
    Thus, it should be
    for loop i in range(2,4)

--- Interview Questions solutions/count primes/index.py
+++ Interview Questions solutions/count primes/index.py
@@ -6,7 +6,7 @@ class Solution:
         isPrime = [True]*n
         isPrime[0] = isPrime[1] = False
         
-        for i in range(2,math.ceil(math.sqrt(n))):
+        for i in range(2,math.ceil(math.sqrt(n)) + 1):
             if isPrime[i]:
                 for multiples_of_i in range(i*i,n,i):
                     isPrime[multiples_of_i] = False
